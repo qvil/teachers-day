c1bae2c41f1e3e66d48f3426d85fc5ec9adf8a91
Taesu Hyeon <qvil1127@gmail.com>
2019-05-09 00:23:50 +0900

    Static file serving test, Add static/favicon.ico

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,9 @@ const StyledDiv = styled.div`
 const Home = () => (
   <div>
     <h1>Home</h1>
-    <StyledDiv>Div</StyledDiv>
+    <StyledDiv>Styled components test with server render</StyledDiv>
+    <h2>Static file serving test</h2>
+    <img src="/static/icons8-classroom-48.png" alt="icons8-classroom-48.png" />
   </div>
 );
 
