c8375be04cf1f010041583b8a168254a72d4508b
현태수 <qvil@op.gg>
2019-05-10 15:57:12 +0900

    Error page test, Ignore .vscode

---
+++ pages/1/2/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Index = () => {
+  return <div>2</div>;
+};
+
+export default Index;
---
+++ pages/1/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Index = () => {
+  throw new Error();
+  return <div>1</div>;
+};
+
+export default Index;
